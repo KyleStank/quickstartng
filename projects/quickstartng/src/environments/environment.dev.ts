a3ef4b8936080caac1eb0713145b6f9e80477ccd
Kyle Stankovich <gamemakergml@gmail.com>
2021-06-21 20:03:54 -0400

    Configured base project files.

---
+++ projects/quickstartng/src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+import { IEnviroment } from './environment.interface';
+
+export const environment: IEnviroment = {
+  production: false,
+  local: false
+};
---
+++ projects/quickstartng/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import { IEnviroment } from './environment.interface';
+
+export const environment: IEnviroment = {
+  production: true,
+  local: false
+};
