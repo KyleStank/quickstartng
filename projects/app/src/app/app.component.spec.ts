6a9c4a5264f37266ae0b604c97426c131a903ecd
Kyle Stankovich <gamemakergml@gmail.com>
2021-06-23 18:27:02 -0400

    Renamed default project to 'app'.

--- projects/app/src/app/app.component.spec.ts
+++ projects/app/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'quickstartng'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('quickstartng');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('quickstartng app is running!');
-  });
 });
